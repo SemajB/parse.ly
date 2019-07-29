ce1726df7a451d7acf5bbc745614d1004fdaa6c9
Semaj <semajbarthelemy55@gmail.com>
2019-07-29 09:14:48 -0500

    added playlist button

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -6,15 +6,15 @@ return (
   <nav className="blue accent-3" role="navigation">
     <div className="nav-wrapper">
       <div className="container">
-      <p 
-        id="nav-mobile" 
-        className="right">
-          <Link className="NavLink" to="/topten"
-          >Top 10</Link>
+        <p id="nav-mobile" className="right">
+          <Link className="NavLink" to="/topten">Top 10</Link>
+        </p>
+      <p id="nav-mobile" className="right">
+        <Link to="/playlist"> Playlists </Link>
       </p>
-      <div>
-      <Link to="/music" className="brand-logo center">Find Your Beat</Link></div>
-      <a href="#" data-activates="mobile-demo" className="button-collapse show-on-large"><i className="material-icons">menu</i></a>
+        <div>
+          <Link to="/music" className="brand-logo center">Find Your Beat</Link></div>
+          <a href="#" data-activates="mobile-demo" className="button-collapse show-on-large"><i className="material-icons">menu</i></a>
       </div>
     </div>
   </nav>
