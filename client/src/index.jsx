d6cfd84c473bccf05f008c814f2b5fe2fac71222
Semaj <semajbarthelemy55@gmail.com>
2019-07-29 09:13:33 -0500

    added routing to /playlist

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,15 +8,17 @@ import {
 import App from './app.jsx';
 import Landing from './Landing.jsx';
 import TopTen from './topten.jsx';
+import Playlist from './Playlist.jsx';
 
 const routing = (
   <Router>
-  <Switch>
-    <React.Fragment>
-      <Route exact path="/" component={Landing} />
-      <Route path="/music" component={App} />
-      <Route path="/topten" component={TopTen} />
-    </React.Fragment>
+    <Switch>
+      <React.Fragment>
+        <Route exact path="/" component={Landing} />
+        <Route path="/music" component={App} />
+        <Route path="/topten" component={TopTen} />
+        <Route path="/playlist" component={Playlist} />
+      </React.Fragment>
     </Switch>
   </Router>
 );
